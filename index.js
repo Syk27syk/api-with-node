73f09f9b8bf94a3e4ba9a496c7de8f7eb44e1fd3
shiyunkhoo <shiyun.khoo@gmail.com>
2022-08-13 18:41:12 +0800

    post end point

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const app = require('express')();
 const PORT = 8080;
 
-/*
 app.listen(
     PORT,
     () => console.log(`it's alive on http://localhost:${PORT}`)
 )
-*/
 
 app.get('/tshirt', () => {
     resizeBy.status(200).send({
         tshirt: 'SHIRT',
         size: 'large'
     })
+});
+
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'We need a logo!' })
+    }
+
+    res.send({
+        tshirt: `SHIRT with your ${logo} and ID of ${id}`,
+    });
 });
